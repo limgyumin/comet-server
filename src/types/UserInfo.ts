a6318c72824cfaaae43cdae5d6748ce265f14c9e
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-21 08:33:45 +0900

    [ADD] UserInfo Types

---
+++ src/types/UserInfo.ts
@@ -0,0 +1,10 @@
+export default interface UserInfoType {
+  id: string;
+  profile: string;
+  bio: string;
+  total: number;
+  today: number;
+  week: number;
+  weekAvg: number;
+  message: string;
+}
