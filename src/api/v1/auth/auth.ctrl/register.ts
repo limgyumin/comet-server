6d6d715fa6acd307234d50c79de8a2d51c581de3
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-20 21:04:38 +0900

    [FIX] Create User Info

--- src/api/v1/auth/auth.ctrl/register.ts
+++ src/api/v1/auth/auth.ctrl/register.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
 
+import { getRepository } from "typeorm";
+import { User } from "../../../../entity/User";
+
 import getAPI from "../../../../lib/githubAPI/getAPI";
 import calContributions from "../../../../lib/contributions/calContributions";
-import createUserInfo from "../../../../lib/database/createUserInfo";
 
 export default async (req: Request, res: Response) => {
   const { body } = req;
@@ -16,12 +18,29 @@ export default async (req: Request, res: Response) => {
     });
 
     const contributions = calContributions(data);
-    createUserInfo(data, contributions);
+
+    const userRepo = getRepository(User);
+    const user: User = new User();
+
+    user.user_id = data.user.login;
+    user.profile = data.user.avatarUrl;
+    user.bio = data.user.bio;
+    user.total_commit = contributions.total;
+    user.today_commit = contributions.today;
+    user.week_commit = contributions.week;
+    user.week_avg = contributions.weekAvg;
+
+    await userRepo.save(user);
 
     res.status(200).json({
       status: 200,
       message: "조회 성공.",
-      data: {},
+      data: {
+        total: contributions.total,
+        today: contributions.today,
+        week: contributions.week,
+        weekAvg: contributions.weekAvg,
+      },
     });
   } catch (error) {
     console.log("서버 오류", error.message);
