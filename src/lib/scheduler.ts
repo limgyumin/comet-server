ce28df4f1b7de62994912befac43f75ae90b73cb
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-21 19:57:22 +0900

    [ADD] Check Is DB Empty

--- src/lib/scheduler.ts
+++ src/lib/scheduler.ts
@@ -1,8 +1,23 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import { User } from "../entity/User";
 import * as schedule from "node-schedule";
 
-export default async () => {
+import getAPI from "./githubAPI/getAPI";
+
+export default () => {
   console.log("[Schedule] Run at a specific time");
-  schedule.scheduleJob("0 * * * * *", () => {
-    console.log("Schedule Executed");
+  schedule.scheduleJob("0 0 12,16,20 * * *", async () => {
+    try {
+      const userRepo = getRepository(User);
+      const count: number = await userRepo.count();
+
+      if (count === 0) {
+        console.log("[GitHubAPI] Empty DB Detected. Exit.");
+        return;
+      }
+    } catch (error) {
+      console.log("[GitHubAPI] :", error.message);
+    }
   });
 };
