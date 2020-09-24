0550051c68f7c9352254dcb0c67eec43eabadb53
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-24 11:20:59 +0900

    [FIX] Create UserInfo In Get API Method

--- src/api/v1/auth/auth.ctrl/register.ts
+++ src/api/v1/auth/auth.ctrl/register.ts
@@ -18,33 +18,19 @@ export default async (req: Request, res: Response) => {
   try {
     const userRepo = getRepository(User);
     const user = await userRepo.findOne({ user_id: userId.toLowerCase() });
-    let data: UserDataType;
     let userInfo: UserInfoType;
 
     //request user가 없으면 새로 조회,
     //이미 있으면 db에 저장된 데이터를 보여줘요.
     if (!user) {
       try {
-        data = await getAPI(userId);
+        userInfo = await getAPI(userId);
       } catch (err) {
         return res.status(404).json({
           status: 404,
           message: "존재하지 않는 아이디.",
         });
       }
-      const contributions = calContributions(data);
-      //db에 저장을 해야하기 때문에
-      //userInfo에 새로 얻은 데이터를 담아요.
-      userInfo = {
-        id: userId.toLowerCase(),
-        profile: data.user.avatarUrl,
-        bio: data.user.bio,
-        total: contributions.total,
-        today: contributions.today,
-        week: contributions.week,
-        weekAvg: contributions.weekAvg,
-        message: "처음 조회된 유저 정보.",
-      };
       createNewData(userInfo);
     } else {
       //userInfo에 db에 저장된 데이터를 담아요.
--- src/lib/githubAPI/getAPI.ts
+++ src/lib/githubAPI/getAPI.ts
@@ -1,5 +1,8 @@
 import "dotenv/config";
 import { GraphQLClient, gql } from "graphql-request";
+import UserDataType from "../../types/UserData";
+import UserInfoType from "../../types/UserInfo";
+import calContributions from "../contributions/calContributions";
 
 export default async (userId: string) => {
   const endpoint = process.env.GITHUB_API_URL;
@@ -31,6 +34,23 @@ export default async (userId: string) => {
     }
   }`;
 
-  const data = await graphQLClient.request(query);
-  return data;
+  const data: UserDataType = await graphQLClient.request(query);
+
+  //data 내의 contributions를 계산하기 위한 함수에요.
+  const contributions = calContributions(data);
+
+  //db에 저장을 해야하기 때문에
+  //userInfo에 새로 얻은 데이터를 담아요.
+  const userInfo: UserInfoType = {
+    id: userId.toLowerCase(),
+    profile: data.user.avatarUrl,
+    bio: data.user.bio,
+    total: contributions.total,
+    today: contributions.today,
+    week: contributions.week,
+    weekAvg: contributions.weekAvg,
+    message: "처음 조회된 유저 정보.",
+  };
+
+  return userInfo;
 };
