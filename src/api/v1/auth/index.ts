a86b94a15b964b8cea9be287809bdc73e9622bcb
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-26 19:48:36 +0900

    [ADD] GitHub OAuth

--- src/api/v1/auth/index.ts
+++ src/api/v1/auth/index.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import register from "./auth.ctrl/register";
+import login from "./login";
 
 const router = Router();
 
-router.post("/register", register);
+router.post("/login", login);
 
 export default router;
