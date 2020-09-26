19fa6aad5945df83530c6c943d53e75b6b2323f5
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-26 19:47:11 +0900

    [UPDATE] Router

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import auth from "./auth";
+import user from "./user";
 
 const router = Router();
 
+router.use("/user", user);
 router.use("/auth", auth);
 
 export default router;
