bfa89897049cf813113454143ad286960336a658
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-28 14:00:15 +0900

    [ADD] check username

---
+++ src/api/v1/github/user.ts
@@ -0,0 +1,68 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import { User } from "../../../entity/User";
+import UserInfoType from "../../../types/UserInfo";
+
+import getAPI from "../../../lib/githubAPI/getAPI";
+import createNewData from "../../../lib/createNewData";
+
+export default async (req: Request, res: Response) => {
+  type RequestBody = {
+    username: string;
+    userId: string;
+  };
+
+  const { username, userId }: RequestBody = req.body;
+
+  try {
+    const userRepo = getRepository(User);
+    const user = await userRepo.findOne({ user_id: userId.toLowerCase() });
+    let userInfo: UserInfoType;
+
+    //request user가 없으면 새로 조회,
+    //이미 있으면 db에 저장된 데이터를 보여줘요.
+    if (!user) {
+      try {
+        userInfo = await getAPI(userId);
+      } catch (err) {
+        return res.status(404).json({
+          status: 404,
+          message: "존재하지 않는 아이디.",
+        });
+      }
+
+      if (username !== null) {
+        userInfo["name"] = username;
+      }
+
+      createNewData(userInfo);
+    } else {
+      //userInfo에 db에 저장된 데이터를 담아요.
+      userInfo = {
+        name: user.name,
+        id: user.user_id,
+        profile: user.profile,
+        bio: user.bio,
+        total: user.total_commit,
+        today: user.today_commit,
+        week: user.week_commit,
+        weekAvg: user.week_avg,
+        message: "재 조회된 유저 정보.",
+      };
+    }
+
+    //response로 새로 조회면 새 정보를, 재 조회면 db의
+    //정보를 보여줘요.
+    res.status(200).json({
+      status: 200,
+      message: "조회 성공.",
+      data: userInfo,
+    });
+  } catch (error) {
+    console.log("서버 오류", error);
+    return res.status(500).json({
+      status: 500,
+      message: "서버 오류.",
+    });
+  }
+};
